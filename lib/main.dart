18933cc077e3d6468c1d572c3cfb1c8b89ae0625
lokishengoku <dthuanle.20@gmail.com>
2021-10-21 15:42:31 +0700

    state management (provider)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:ui';
 
+import 'package:demo_shop_app/providers/products.dart';
+import 'package:demo_shop_app/screens/product_detail_screen.dart';
 import 'package:demo_shop_app/screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,23 +14,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          textTheme: TextTheme(
-            bodyText2: TextStyle(
-                fontFamily: 'OpenSans', fontSize: 14, color: Colors.white),
-            bodyText1: TextStyle(
-                fontFamily: 'OpenSans', fontSize: 14, color: Colors.black),
-            headline6: TextStyle(
-                fontFamily: 'SourceSansPro',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white),
-          )),
-      home: ProductsOverviewScreen(),
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            textTheme: TextTheme(
+              bodyText2: TextStyle(
+                  fontFamily: 'OpenSans', fontSize: 14, color: Colors.white),
+              bodyText1: TextStyle(
+                  fontFamily: 'OpenSans', fontSize: 14, color: Colors.black),
+              headline6: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            )),
+        // home: ProductsOverviewScreen(),
+        initialRoute: '/',
+        routes: {
+          '/': (ctx) => ProductsOverviewScreen(),
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
