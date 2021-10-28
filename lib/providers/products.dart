95c5e6a0718ef84d4ee5848b0059e27bdc4a5148
lokishengoku <dthuanle.20@gmail.com>
2021-10-28 21:23:05 +0700

    products management: delete product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -69,4 +69,9 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((element) => element.id == id);
+    notifyListeners();
+  }
 }
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
+import 'package:demo_shop_app/providers/products.dart';
 import 'package:demo_shop_app/screens/edit_product_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -31,7 +33,9 @@ class UserProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor),
           IconButton(
             icon: Icon(Icons.delete),
-            onPressed: () {},
+            onPressed: () {
+              Provider.of<Products>(context, listen: false).deleteProduct(id);
+            },
           )
         ]),
       ),
