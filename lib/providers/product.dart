7f2bb9a6e72ed5ef30de56732ee54814b0ae9925
lokishengoku <dthuanle.20@gmail.com>
2021-10-30 21:01:20 +0700

    firebase real-time database: toggle isFavorite

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:demo_shop_app/models/http_exception.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +20,25 @@ class Product with ChangeNotifier {
       required this.imageUrl,
       this.isFavorite = false});
 
-  void toggleFavoriteState() {
+  Future<void> toggleFavoriteState() async {
+    bool oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = Uri.parse(
+        'https://flutter-shop-app-demo-6b516-default-rtdb.asia-southeast1.firebasedatabase.app/products/${id}.json');
+
+    try {
+      final response =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+      if (response.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+        throw HttpException('couldn\'t toggle favorite this product');
+      }
+    } catch (err) {
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
   }
 }
